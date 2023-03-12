3f3f4f237bbdc337bc5913696234d7742e2ddbf4
YevheniyaSolomyana <72937387+YevheniyaSolomyana@users.noreply.github.com>
2023-03-12 19:05:15 -0400

    Project 3

---
+++ flashcards-pt2/src/cardsArray.jsx
@@ -0,0 +1,23 @@
+const cardsArray = [
+  {
+    "question": "Why do computer scientists get confused between Halloween and Christmas?",
+    "answer": "because oct 31 = dec 25"
+  },
+  {
+    "question": "How do robots eat pizza?",
+    "answer": "one byte at a time"
+  },
+  {
+    "question": "How easy is it to count in binary?",
+    "answer": "it's as easy as 01 10 11"
+  },
+  {
+    "question": "How did the first program die?",
+    "answer": "it was executed"
+  },
+  {
+    "question": "How many software engineers does it take to change a light bulb?",
+    "answer": "it can't be done, it's a hardware problem"
+  }
+]
+export default cardsArray;
