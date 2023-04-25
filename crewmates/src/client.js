71635f0c32cc7409057c0a4508ce88d8b6239999
YevheniyaSolomyana <72937387+YevheniyaSolomyana@users.noreply.github.com>
2023-04-25 04:36:15 +0200

    Delete crewmates directory

--- crewmates/src/client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const URL = 'https://lvgflwvqtuaqzurhvkuu.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2Z2Zsd3ZxdHVhcXp1cmh2a3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEwNTc3MjcsImV4cCI6MTk5NjYzMzcyN30.n-wLZIrryZxSFPbbYOXMn-wbhEQGV063iGk_FaIeoGA';
-
-export const supabase = createClient(URL, API_KEY);
